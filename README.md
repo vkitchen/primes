ce6f23db0ce33f54b3f274da9a070336836c95be
Vaughan Kitchen <v.kitchen@gnoms.org>
2019-07-11 10:44:17 +1200

    Better eratosthenes implementation

--- README.md
+++ README.md
@@ -13,5 +13,6 @@ Time to complete for the primes up to 2,000,000,000
 | div-by-prime | 59m48s |
 | eratosthenes | 206s |
 | eratosthenes-bitwise | 56s |
+| eratosthenes-bitwise-faster | 18s |
 
 N.B. Not all algorithms are fully optimised
---
+++ eratosthenes-bitwise-faster.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+// https://stackoverflow.com/questions/2525310/how-to-define-and-work-with-an-array-of-bits-in-c
+#define SetBit(A,k)     ( A[(k/8)] |= (1 << (k%8)) )
+#define TestBit(A,k)    ( ( A[(k/8)] & (1 << (k%8)) ) == 0 )
+
+#define TOP 2000000000
+
+char primes[TOP/8];
+
+void eratosthenes() {
+	SetBit(primes, 0);
+	SetBit(primes, 1);
+
+	long end = sqrt(TOP);
+	for (long i = 2; i < end; i++)
+		if (TestBit(primes, i))
+			for (long j = i * i; j <= TOP; j += i)
+				SetBit(primes, j);
+}
+
+void print_largest() {
+	for (long i = TOP; i > 0; i--)
+		if (TestBit(primes, i)) {
+			printf("Largest prime: %ld\n", i);
+			return;
+		}
+}
+
+void print_count() {
+	long count = 0;
+	for (long i = 0; i < TOP; i++)
+		if (TestBit(primes, i))
+			count++;
+	printf("Number of primes: %ld\n", count);
+}
+
+int main(int argc, char **argv) {
+	eratosthenes();
+	print_largest();
+	print_count();
+	return 0;
+}
